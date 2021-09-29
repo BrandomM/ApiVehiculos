6374eb104271850e219a84f0e33a309270903926
Brandom Mosquera <bcmc-97@hotmailo.com>
2021-09-29 10:31:49 -0500

    Inicio del proyecto

---
+++ src/main/java/com/apivehiculos/apivehiculos/ApivehiculosApplication.java
@@ -0,0 +1,13 @@
+package com.apivehiculos.apivehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApivehiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApivehiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apivehiculos/apivehiculos/ApivehiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apivehiculos.apivehiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApivehiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
